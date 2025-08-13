0361f6e2dc0edd5f265c7c29598158badd722bf7
VASUDEV KUMAR <VASUDEVSHANU07@GMAIL.COM>
2025-08-13 23:01:42 +0530

    Update AddNewDoctor.jsx

--- dashboard/src/components/AddNewDoctor.jsx
+++ dashboard/src/components/AddNewDoctor.jsx
@@ -58,7 +58,7 @@ const AddNewDoctor = () => {
       formData.append("doctorDepartment", doctorDepartment);
       formData.append("docAvatar", docAvatar);
       await axios
-        .post("https://hospital-management-system-p34w.onrender.com//api/v1/user/doctor/addnew", formData, {
+        .post("https://hospital-management-system-p34w.onrender.com/api/v1/user/doctor/addnew", formData, {
           withCredentials: true,
           headers: { "Content-Type": "multipart/form-data" },
         })
