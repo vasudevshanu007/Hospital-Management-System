986f02853f5108e5ea99d28f7af04edc8712bc09
VASUDEV KUMAR <VASUDEVSHANU07@GMAIL.COM>
2025-08-13 21:24:31 +0530

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const dbConnection = () => {
-mongoose.connect(MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
 
