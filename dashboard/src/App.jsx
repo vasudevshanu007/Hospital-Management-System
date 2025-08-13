4efda03e5f56ea5cf2d22a13ee934cd4779e5bca
VASUDEV KUMAR <VASUDEVSHANU07@GMAIL.COM>
2025-08-13 21:45:58 +0530

    Update App.jsx

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:5000/api/v1/user/admin/me",
+          "https://hospital-management-system-p34w.onrender.com//api/v1/user/admin/me",
           {
             withCredentials: true,
           }
