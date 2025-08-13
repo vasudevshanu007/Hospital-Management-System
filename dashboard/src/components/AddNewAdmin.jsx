73f9a769f1e224443f32fcff2033164f6d9856fc
VASUDEV KUMAR <VASUDEVSHANU07@GMAIL.COM>
2025-08-13 21:46:34 +0530

    Update AddNewAdmin.jsx

--- dashboard/src/components/AddNewAdmin.jsx
+++ dashboard/src/components/AddNewAdmin.jsx
@@ -23,7 +23,7 @@ const AddNewAdmin = () => {
     try {
       await axios
         .post(
-          "http://localhost:5000/api/v1/user/admin/addnew",
+          "https://hospital-management-system-p34w.onrender.com//api/v1/user/admin/addnew",
           { firstName, lastName, email, phone, nic, dob, gender, password },
           {
             withCredentials: true,
