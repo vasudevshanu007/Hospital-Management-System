0d2a78fdc52f5cec2b9d3fd0b0b82d453ba2de07
VASUDEV KUMAR <VASUDEVSHANU07@GMAIL.COM>
2025-08-13 23:02:52 +0530

    Update Sidebar.jsx

--- dashboard/src/components/Sidebar.jsx
+++ dashboard/src/components/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
 
   const handleLogout = async () => {
     await axios
-      .get("https://hospital-management-system-p34w.onrender.com//api/v1/user/admin/logout", {
+      .get("https://hospital-management-system-p34w.onrender.com/api/v1/user/admin/logout", {
         withCredentials: true,
       })
       .then((res) => {
