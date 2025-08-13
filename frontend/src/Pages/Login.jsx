c3513821e18677de70b25edea0f37f503519f0f5
VASUDEV KUMAR <VASUDEVSHANU07@GMAIL.COM>
2025-08-13 21:36:32 +0530

    Update Login.jsx

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     try {
       await axios
         .post(
-          "http://localhost:5000/api/v1/user/login",
+          "https://hospital-management-system-p34w.onrender.com//api/v1/user/login",
           { email, password, confirmPassword, role: "Patient" },
           {
             withCredentials: true,
