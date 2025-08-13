52332007f8f468fd199d304ee97979b31aacb040
VASUDEV KUMAR <VASUDEVSHANU07@GMAIL.COM>
2025-08-13 21:41:19 +0530

    Update MessageForm.jsx

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -14,7 +14,7 @@ const MessageForm = () => {
     try {
       await axios
         .post(
-          "http://localhost:5000/api/v1/message/send",
+          "https://hospital-management-system-p34w.onrender.com//api/v1/message/send",
           { firstName, lastName, email, phone, message },
           {
             withCredentials: true,
